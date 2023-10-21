c7a1c2c7bc6d93dd585dea0008c67f9633e01eca
tusshar17 <tr.jakhara019@gmail.com>
2023-10-21 18:29:50 +0530

    Upper Bound

---
+++ BinarySearch_1D/src/UpperBound.java
@@ -0,0 +1,34 @@
+public class UpperBound {
+
+    //arr[ans]>=x
+
+    static int findUpperBound(int arr[], int x){
+
+        int n = arr.length;
+        int ans = n+1;
+        int low = 0, high = n-1;
+
+        while (low<=high){
+            int mid = (low+high)/2;
+
+            if (arr[mid]>x){
+                ans = mid;
+                high = mid-1;
+            } else low = mid+1;
+        }
+
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {3,5,8,9,15,19};
+        int x = 9;
+
+        int res = findUpperBound(arr, x);
+
+        System.out.println("Upper Bound of " + x + " is : " + arr[res] + " at index " + res);
+
+    }
+}
