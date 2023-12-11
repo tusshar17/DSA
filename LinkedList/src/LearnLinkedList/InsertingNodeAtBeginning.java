04ee8b07146c603de4f903fbd8f99ca8397391b0
tusshar17 <tr.jakhara019@gmail.com>
2023-12-11 22:54:04 +0530

    insertung a node at beginning

---
+++ LinkedList/src/LearnLinkedList/InsertingNodeAtBeginning.java
@@ -0,0 +1,59 @@
+package LearnLinkedList;
+
+
+class ListNode{
+    int data;
+    ListNode next;
+
+    ListNode(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class InsertingNodeAtBeginning {
+
+    static ListNode insertAtFirst(int val, ListNode head){
+
+        // Step 1 - creating a new node
+        ListNode newNode  = new ListNode(val);
+
+        // Step 2 - point newNode to head of the LinkedList
+        newNode.next = head;
+
+        // Step 3 - make newNode as the head of LinkedList
+        head = newNode;
+
+        return head;
+    }
+
+
+    static void printList(ListNode head){
+        ListNode current = head;
+        while (current.next!=null){
+            System.out.print(current.data + "-->");
+            current = current.next;
+        }
+        System.out.println("end");
+    }
+
+
+    public static void main(String[] args) {
+        ListNode head = null;
+
+        head = insertAtFirst(40, head);
+        head = insertAtFirst(30, head);
+        head = insertAtFirst(20, head);
+        head = insertAtFirst(10, head);
+
+        System.out.println("LinkedList before inserting a zero in beginning");
+        printList(head);
+
+        head = insertAtFirst(0, head);
+
+        System.out.println("LinkedList after inserting a zero in beginning");
+        printList(head);
+    }
+
+
+}
