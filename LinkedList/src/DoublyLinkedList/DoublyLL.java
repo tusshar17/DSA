6c7392fb913f5ecd6b10218d4cb5135881c68975
tusshar17 <tr.jakhara019@gmail.com>
2023-12-21 06:46:52 +0530

    Doubly LinkedList

---
+++ LinkedList/src/DoublyLinkedList/DoublyLL.java
@@ -0,0 +1,54 @@
+package DoublyLinkedList;
+
+
+class Node{
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    Node(int data, Node next, Node prev){
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+public class DoublyLL {
+
+    private static void printList(Node head){
+        while (head!=null){
+            System.out.print(head.data);
+            System.out.print("->");
+            head = head.next;
+        }
+        System.out.println("END");
+    }
+
+    private static Node convertArrayToDLL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node prev = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i], null, prev);
+            prev.next = temp;
+            prev = temp;
+        }
+
+        return head;
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        Node head = convertArrayToDLL(arr);
+        printList(head);
+    }
+
+
+}
