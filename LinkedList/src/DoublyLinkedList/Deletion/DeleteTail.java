c2c7c6e2aa02510cbc774d018ad22ed761d6005e
tusshar17 <tr.jakhara019@gmail.com>
2023-12-21 22:49:21 +0530

    delete tail of DLL

--- LinkedList/src/DoublyLinkedList/Deletion/DeleteTail.java
+++ LinkedList/src/DoublyLinkedList/Deletion/DeleteTail.java
@@ -1,2 +1,41 @@
-package DoublyLinkedList.Deletion;public class DeleteTail {
+package DoublyLinkedList.Deletion;
+
+import static DoublyLinkedList.Deletion.DeleteHead.printList;
+import static DoublyLinkedList.Deletion.DeleteHead.convertArrayToDLL;
+
+public class DeleteTail {
+
+    static Node deleteTail(Node head){
+
+        if (head==null || head.next==null) return null;
+
+        Node tail = head;
+
+        while (tail.next!=null){
+            tail = tail.next;
+        }
+
+        Node prev = tail.prev;
+        prev.next = null;
+        tail.prev = null;
+
+        return head;
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        Node head = convertArrayToDLL(arr);
+
+        System.out.println("Before deleting tail");
+        printList(head);
+
+        head = deleteTail(head);
+
+        System.out.println("After deleting tail");
+        printList(head);
+    }
+
+
 }
