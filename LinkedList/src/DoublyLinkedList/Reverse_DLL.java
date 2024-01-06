d5b74dc6f1b869e43d34d0098ccd7bbfead11d6f
tusshar17 <tr.jakhara019@gmail.com>
2024-01-07 00:59:03 +0530

    reverse a DLL

--- LinkedList/src/DoublyLinkedList/Reverse_DLL.java
+++ LinkedList/src/DoublyLinkedList/Reverse_DLL.java
@@ -1,2 +1,37 @@
-package DoublyLinkedList;public class Reverse_DLL {
+package DoublyLinkedList;
+
+import static DoublyLinkedList.Insertion.InsertAtHead.convertArrayToDLL;
+
+public class Reverse_DLL {
+
+    static Node reverseDLL(Node head){
+
+        if (head==null || head.next==null) return head;
+
+        Node current = head;
+        Node prevNode = null;
+
+        while (current!=null){
+            prevNode = current.prev;
+            current.prev = current.next;
+            current.next = prevNode;
+
+            current = current.prev;
+        }
+
+        return prevNode.prev;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        Node head = DoublyLL.convertArrayToDLL(arr);
+        System.out.println("Original List:");
+        DoublyLL.printList(head);
+        Node reversedList = reverseDLL(head);
+        System.out.println("Reversed List:");
+        DoublyLL.printList(reversedList);
+    }
+
+
 }
