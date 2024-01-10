3f4e87b1034262e0cf692d5995739a77284cb715
tusshar17 <tr.jakhara019@gmail.com>
2024-01-11 02:25:18 +0530

    Delete Middle Node of LL

--- LinkedList/src/MediumProblems/DeleteMiddleNode.java
+++ LinkedList/src/MediumProblems/DeleteMiddleNode.java
@@ -1,2 +1,40 @@
-package MediumProblems;public class DeleteMiddleNode {
+package MediumProblems;
+
+public class DeleteMiddleNode {
+
+    static Node deleteMiddleNode(Node head){
+
+        if (head==null || head.next==null){
+                return null;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        Node prevOfMiddle = null;
+
+        while (fast!=null && fast.next!=null){
+            prevOfMiddle = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prevOfMiddle.next = slow.next;
+
+
+        return head;
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6};
+
+        Node head = LinkedList.convertArrayToLL(arr);
+        System.out.println("Original List:");
+        LinkedList.printList(head);
+
+        System.out.println("After deleting middle node:");
+        head = deleteMiddleNode(head);
+        LinkedList.printList(head);
+    }
 }
