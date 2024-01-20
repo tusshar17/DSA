cee0e5b1739fc3c605a83fe9ff34d4ed618fe31e
tusshar17 <tr.jakhara019@gmail.com>
2024-01-21 03:20:17 +0530

    Combination sum 2

---
+++ Recursion/src/Subsequences/CombinationSumUnique.java
@@ -0,0 +1,37 @@
+package Subsequences;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSumUnique {
+
+    static void findCombinations(int index, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds){
+        if (target == 0) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for (int i=index; i<arr.length; i++){
+            if (i>index && arr[i]==arr[i-1]) continue;
+            if (arr[i]>target) break;
+
+            ds.add(arr[i]);
+            findCombinations(i+1, arr, target-arr[i], ans, ds );
+            ds.remove(ds.size()-1);
+        }
+    }
+
+    static List<List<Integer>> combinationSum2(int[] candidates, int target){
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        findCombinations(0, candidates, target, ans, new ArrayList<>());
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int v[] = {10, 1, 2, 7, 6, 1, 5};
+        List<List<Integer>> comb = combinationSum2(v, 8);
+        System.out.println(comb.toString().replace(",", " "));
+    }
+}
